d1121b7b7426c4de345fb5c3ad9bb2724ee2ec12
Greg Linton <lintong@pagodabox.com>
2015-09-04 18:05:20 -0400

    Breaking out into multiple lines with lyon

--- hookit/hooks/default-start.rb
+++ hookit/hooks/default-start.rb
@@ -3,22 +3,46 @@
 service "cache" do
   action :enable
   init :runit
-  only_if { ( File.exist?('/etc/service/cache/run') ) && ( 6.times { `sv status cache`; $?.exitstatus == 0 && break; sleep 1; }; $?.exitstatus == 0 ) }
+  only_if do
+    if File.exist?('/etc/service/cache/run')
+      6.times { `sv status cache`; break if $?.exitstatus == 0; sleep 1}
+      $?.exitstatus == 0
+    else
+      false
+    end
+  end
 end
 
 service "sentinel" do
   action :enable
-  only_if { ( File.exist?('/etc/service/sentinel/run') ) && ( 6.times { `sv status sentinel`; $?.exitstatus == 0 && break; sleep 1; }; $?.exitstatus == 0 ) }
   init :runit
+  only_if do
+    if File.exist?('/etc/service/sentinel/run')
+      6.times { `sv status sentinel`; break if $?.exitstatus == 0; sleep 1}
+      $?.exitstatus == 0
+    else
+      false
+    end
+  end
 end
 
 service "proxy" do
   action :enable
-  only_if { ( File.exist?('/etc/service/proxy/run') ) && ( 6.times { `sv status proxy`; $?.exitstatus == 0 && break; sleep 1; }; $?.exitstatus == 0 ) }
   init :runit
+  only_if do
+    if File.exist?('/etc/service/proxy/run')
+      6.times { `sv status proxy`; break if $?.exitstatus == 0; sleep 1}
+      $?.exitstatus == 0
+    else
+      false
+    end
+  end
 end
 
 execute 'ensure socket' do
   command "netstat -an | egrep ':(6379|6380)'"
-  only_if { 5.times { `netstat -an | egrep ':(6379|6380)'`; $?.exitstatus == 0 && break; sleep 1; } }
+  only_if do
+    5.times { `netstat -an | egrep ':(6379|6380)'`; $?.exitstatus == 0 && break; sleep 1 }
+    $?.exitstatus == 0
+  end
 end
