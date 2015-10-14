c77b31b817ea73339f5a780bdeb6d9e9aa2912a6
Daniel Barney <daniel@pagodabox.com>
2015-10-14 11:14:40 -0600

    removing the configure hook

--- tests/configure_test.sh
+++ tests/configure_test.sh
@@ -1,11 +1,8 @@
 VERSION=$1
-echo running tests for $VERSION of postgresql
+echo running tests for $VERSION of redis
 UUID=$(cat /proc/sys/kernel/random/uuid)
 
 pass "unable to start the $VERSION container" docker run --privileged=true -d --name $UUID nanobox/redis:$VERSION
 defer docker kill $UUID
-
-# we should be able to run the basic configure hook
-pass "unable to run the configure hook for $VERSION" docker exec $UUID /opt/bin/default-configure "$PAYLOAD"
 
 # now we just need to verify that changes were atually made correctly.
