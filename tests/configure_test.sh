738eec6e2ed14a4df627cecf959d5166cd6328fd
Braxton Huggins <braxton@pagodabox.com>
2016-02-01 10:33:40 -0700

    update payload in test, run update hook before the configure hook

--- tests/configure_test.sh
+++ tests/configure_test.sh
@@ -1,8 +1,16 @@
 VERSION=$1
 echo running tests for $VERSION of redis
 UUID=$(cat /proc/sys/kernel/random/uuid)
+PAYLOAD='{"boxfile":{"name":"cache1","stability":"stable","version":'${VERSION}'},"logtap_host":"127.0.0.1","platform":"local","uid":"redis1"}'
+echo boxfile: "$PAYLOAD"
 
 pass "unable to start the $VERSION container" docker run --privileged=true -d --name $UUID nanobox/redis:$VERSION
 defer docker kill $UUID
+
+# we should be able to run the basic update hook
+pass "unable to run the update hook for $VERSION" docker exec $UUID /opt/bin/update "$PAYLOAD"
+
+# we should be able to run the basic configure hook
+pass "unable to run the configure hook for $VERSION" docker exec $UUID /opt/bin/default-configure "$PAYLOAD"
 
 # now we just need to verify that changes were atually made correctly.
